499fdf0ac2c8c44ba6d0e34b914f537262fa22c3
Melody Stern <melliemuse@gmail.com>
2020-02-25 10:13:53 -0600

    Set up file structure, created nav bar, rendering home nav bar and application views

---
+++ src/components/nav/Nav.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function nav() {
+    return (
+        <>
+            <ul>
+                <li><Link to=''>Home</Link></li>
+                <li><Link to=''>Profile</Link></li>
+                <li><Link to=''>Sell</Link></li>
+                <li><Link to=''>My Products</Link></li>
+                <li><Link to=''>Reports</Link></li>
+                <li><Link to=''>Recommendations</Link></li>
+                <li><Link to=''>Favorites</Link></li>
+                <li><Link to=''>Shopping Cart</Link></li>
+                <li><Link to=''>Register</Link></li>
+                <li><Link to=''>Login</Link></li>
+            </ul>   
+        </>
+    )
+}
