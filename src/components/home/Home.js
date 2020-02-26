1802121bc7c428ede9d4d3ec0f270096d2895bae
Cherkesky <guy@cherkesky.com>
2020-02-26 11:53:20 -0600

    latest products only show if auth

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,23 +1,28 @@
 import React, { useState, useEffect } from 'react'
 import APIManager from '../helpers/APIManager'
 import ProductItem from '../products/ProductItem'
+import { isAuthenticated } from '../helpers/simpleAuth';
 
 export default function Home() {
 
     const [productList, setProductList] = useState([]);
 
+
     const getProductList = () => {
-        APIManager.getAll('products?limit=20')
+        if (isAuthenticated()) {
+            APIManager.getAll('products?limit=20')
                 .then(setProductList)
-        };
 
-        useEffect(getProductList, [])
+        }
+    };
 
-        return (
-            <>
-                <h1>Latest 20 Products</h1>
-                {productList.map((item) => <ProductItem key={item.id} item={item} />)}
+    useEffect(getProductList, [])
 
-            </>
-        )
-    }
+    return (
+        <>
+            {isAuthenticated() && <h1>Latest 20 Products</h1>}
+            {productList.map((item) => <ProductItem key={item.id} item={item} />)}
+
+        </>
+    )
+}
