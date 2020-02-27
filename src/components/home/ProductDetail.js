b46f2e2d2db0280646aee49d3a8fede0e4ac34e9
Melody Stern <melliemuse@gmail.com>
2020-02-27 14:00:45 -0600

    added comments for steps to complete to check for open orders and add product to cart

--- src/components/home/ProductDetail.js
+++ src/components/home/ProductDetail.js
@@ -4,6 +4,11 @@ import APIManager from '../helpers/APIManager'
 
 export default function ProductDetail(props) {
 
+
+    // add product to order - logic check for open order if there is none  create it the can be only one
+    // from product details
+    // first write a get for order by customer where there is no attached payment type
+
     const [productdetail, setProductDetail] = useState({
         product_type: {}
     })
@@ -16,13 +21,14 @@ export default function ProductDetail(props) {
     }, [])
     return (
         <>
-            <h1>{productdetail.name}</h1> 
+            <h1>{productdetail.name}</h1>
             <p>{productdetail.price}</p>
             <p>{productdetail.description}</p>
             <p>{productdetail.quantity}</p>
             <p>{productdetail.location}</p>
-            <p>{productdetail.image_path}</p> 
+            <p>{productdetail.image_path}</p>
             <p>{productdetail.product_type.name}</p>
+            <button>Add Item to Cart</button>
         </>
     )
 }
