06a527133a180a8b5d6c7d1754625604fcfae4cc
Melody Stern <melliemuse@gmail.com>
2020-02-27 10:06:23 -0600

    rerouted product details from home

--- src/components/products/ProductItem.js
+++ src/components/products/ProductItem.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function ProductItem(props) {
   return (
     <>
-    <a href={props.item.url}> •Name: {props.item.name} •Created At: {props.item.created_at}</a> <br/>
+    {/* <a href={props.item.url}> •Name: {props.item.name} •Created At: {props.item.created_at}</a> <br/> */}
+    •Name: <Link to={`/products/${props.item.id}`}>{props.item.name}</Link>  •Created At: {props.item.created_at} <br/>
     </>
   )
 }
