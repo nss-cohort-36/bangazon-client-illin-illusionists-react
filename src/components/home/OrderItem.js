6c6c425d15578891a87ff20fa12c96c1b046d763
Ken Boyd <klb417@gmail.com>
2020-03-04 15:05:31 -0600

    showing products and complete/cancel buttons on orders

--- src/components/home/OrderItem.js
+++ src/components/home/OrderItem.js
@@ -1,20 +1,23 @@
-import React, { Component } from 'react'
-import './OrderItem.css'
+import React, { Component } from "react";
+import "./OrderItem.css";
+import ProductItem from "./ProductItem";
 
 export default class Order extends Component {
-
-    render() {
-      console.log('props in the order', this.props)
-        return (
-           <div className="order-card">
-               <div className="order-card-body">
-                   <h4 className="order-card-heading" >
-                      Created At
-                   </h4>
-                    <p className="order-card-description">{this.props.created}</p>
-               </div>
-           </div>  
-        )
-    }
-    
+  render() {
+    return (
+      <div className="order-card">
+        <div className="order-card-body">
+          <h4 className="order-card-heading">Created At</h4>
+          <p className="order-card-description">{this.props.created}</p>
+          <ol>
+            {this.props.products.map(product => (
+              <li key={product.id}><ProductItem product={product} /></li>
+            ))}
+          </ol>
+          <button className="order-card-complete-button">Complete Order</button>
+          <button className="order-card-cancel-button">Cancel Order</button>
+        </div>
+      </div>
+    );
+  }
 }
--- src/components/home/OrdersItemList.js
+++ src/components/home/OrdersItemList.js
@@ -1,43 +1,67 @@
-import React, { Component } from 'react'
-import Order from './OrderItem'
-import './OrdersItemList.css'
+import React, { Component } from "react";
+import Order from "./OrderItem";
+import "./OrdersItemList.css";
 
 export default class OrdersList extends Component {
-    
-    state = {
-        orders: []
-    }
-    
-    componentDidMount(){
-        this.getAllOrders()
-    }
+  state = {
+    orders: []
+  };
 
-    getAllOrders = () => {
-        fetch('http://localhost:8000/orders', {
-            "method": "GET",
-            "headers": {
-                "Accept": "application/json",
-                "Authorization": `Token ${sessionStorage.getItem("bangazon_token")}`
-            }
-        })
-        .then(response => response.json())
-        .then(response => this.setState({orders:response}))
-    }
-    
-    render() {
-        console.log('ORDERS STATE', this.state);
-        return (
-        <div>
-            <div className="header-container">
-                <h2 className="order-list-header">Recent Orders</h2>
-            </div>
-            <div className="orders-item-list-container">
-                        { this.state.orders.map(order => 
-                            <Order key={order.id} url={order.url} created={order.created_at}/> 
-                        )
-                        }
-            </div>
+  async componentDidMount() {
+    // await this.getAllOrders();
+    await this.getProductsForOrders();
+  }
+
+  getAllOrders = () => {
+    return fetch("http://localhost:8000/orders?self=true", {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        Authorization: `Token ${sessionStorage.getItem("bangazon_token")}`
+      }
+    })
+      .then(response => response.json())
+      // .then(response => this.setState({ orders: response }));
+  };
+
+  getProductsForOrders = async () => {
+    const ordersWithProducts = await this.getAllOrders();
+    for (const order of ordersWithProducts) {
+      const productsInOrderResponse = await fetch(
+        `http://localhost:8000/orderproducts?order=${order.id}`,
+        {
+          method: "GET",
+          headers: {
+            Accept: "application/json",
+            Authorization: `Token ${sessionStorage.getItem("bangazon_token")}`
+          }
+        }
+      );
+      const productsInOrder = await productsInOrderResponse.json();
+      order["products"] = productsInOrder.map(
+        productInOrder => productInOrder.product
+      );
+    };
+    this.setState({ orders: ordersWithProducts });
+  };
+
+  render() {
+    return (
+      <div>
+        <div className="header-container">
+          <h2 className="order-list-header">Recent Orders</h2>
         </div>
-        )
-    }
+        <div className="orders-item-list-container">
+          {this.state.orders.map(order => (
+            <Order
+              key={order.id}
+              url={order.url}
+              created={order.created_at}
+              products={order.products}
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
