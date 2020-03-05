90a902a465d648c24c968002979fa8b68e7d5c7b
Ryan Bishop <rdbishop06@gmail.com>
2020-03-05 11:49:29 -0600

    feat: sub-routing in 'reports' component view

--- src/components/reports/Reports.js
+++ src/components/reports/Reports.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import { Route, Link } from 'react-router-dom'
+import AbandonedProductTypes from './AbandonedProductTypes'
 
 export default function Reports() {
     return (
         <div>
-            REPORTS
+            <h3>Reports to Run</h3>
+            <ul>
+                <li><Link to="/reports/abandonedproducttypes">Abandoned Product Types</Link></li>
+            </ul>
+            <Route exact path="/reports/abandonedproducttypes" render={props => {
+                return <AbandonedProductTypes />
+            }} />
         </div>
     )
 }
