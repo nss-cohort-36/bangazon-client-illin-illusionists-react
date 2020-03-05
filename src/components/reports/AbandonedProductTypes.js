088bcd3361b0abf84bc495facea35f7bee816230
Ryan Bishop <rdbishop06@gmail.com>
2020-03-05 14:25:33 -0600

    feat: abandoned product types component

--- src/components/reports/AbandonedProductTypes.js
+++ src/components/reports/AbandonedProductTypes.js
@@ -2,8 +2,23 @@ import React from 'react'
 
 export default function AbandonedProductTypes() {
     return (
-        <div>
-            ABANDONED
-        </div>
+        <React.Fragment>
+            <table>
+                <thead>
+                    <tr>
+                        <td><strong>Product Type</strong></td>
+                        <td><strong>Incomplete Orders</strong></td>
+                    </tr>
+                </thead>
+                <tbody>
+                    {/* REPLACE INSIDE WITH DYNAMIC DATA */}
+                        <tr>
+                            <td>Electronics</td>
+                            <td>22</td>
+                        </tr>
+                    {/* REPLACE INSIDE WITH DYNAMIC DATA */}
+                </tbody>
+            </table>
+        </React.Fragment>
     )
 }
